4653cfc90dd658517721c10e2d3b65d1def0c5df
simstodd <todd.sims@gmail.com>
2018-01-23 14:44:39 -0500

    Add files via upload

---
+++ bme280testme_only/moon_phases.h
@@ -0,0 +1,288 @@
+// Moon Phase Calendar for Arduino, using Pro Trinket 3V/12MHz
+// Copyright 2015 by Mark Moene
+//https://github.com/MarkMoene/MoonPhaseCalendar
+/*
+
+#include <TM1637Display.h>
+#include <assert.h>
+
+// ----------------------------------------------------------------------------
+// Configuration:
+
+// Start date, must be date valid for moon phase algorithm:
+// from 1-1-1900 to 31-12-2199 inclusive.
+#define START_DATE { 2015, 11, 1 }
+
+// Time showing blank display when updating year or date:
+const int display_blank_pause_ms = 100;
+
+// Configuration: Pins
+
+// Moon phase output (contiguous pins):
+const int pin_moon_first = 9;  // #13: onboard LED
+const int pin_moon_count = 4;
+const int phase_count    = 8;
+
+// Date 4-digit display:
+const int pin_date_sda   = 0;
+const int pin_date_scl   = 1;
+
+// End configuration.
+
+#define dimension_of( a ) ( sizeof(a) / sizeof(0[a]) )
+
+// ----------------------------------------------------------------------------
+// Hardware-related
+
+// Rotary encoder:
+// ....
+
+// Push buttons:
+// ....
+
+// Date display:
+
+using Pause = int;
+
+struct Date
+{
+    int year;
+    int month;
+    int day;
+};
+
+TM1637Display display( pin_date_scl, pin_date_sda );
+
+void setup_date_display()
+{
+    // display constructor sets up the pins
+    const int max_brightness = 0xf;
+
+    display.setBrightness( max_brightness );
+}
+
+void display_pause( Pause const pause_ms )
+{
+    delay( pause_ms );
+}
+
+void display_blank()
+{
+    using length = uint8_t;
+    using pos = uint8_t;
+
+    uint8_t const segs_off   = 0;
+    uint8_t const segments[] = { segs_off, segs_off, segs_off, segs_off };
+
+    display.setSegments( segments, length(4), pos(0) );
+}
+
+void display_number( int const x, Pause const pause_ms )
+{
+    using leading_zeros = bool;
+    using length = uint8_t;
+    using pos = uint8_t;
+
+    display_blank();
+    display_pause( pause_ms );
+    display.showNumberDec( x, leading_zeros(true), length(4), pos(0) );
+}
+
+void display_date( Date const date, Pause const pause_ms )
+{
+    // blank, pause, yyyy, pause, blank, pause, ddmm
+    display_number( date.year, pause_ms );
+    display_pause ( pause_ms );
+    display_number( 100 * date.day + date.month, pause_ms );
+}
+
+// Moon phase display:
+
+int pin_moon( int const i )
+{
+    return pin_moon_first + i;
+}
+
+void setup_moon_phase_display()
+{
+    for ( int i = 0; i < pin_moon_count; ++i )
+    {
+        pinMode( pin_moon(i), OUTPUT );
+    }
+}
+
+void display_moon_phase( int const phase )
+{
+    static int pattern[] =
+    {
+        0b0000,
+        0b0001,
+        0b0011,
+        0b0111,
+        0b1111,
+        0b1110,
+        0b1100,
+        0b1000,
+    };
+    static_assert( dimension_of(pattern) == phase_count, "expecting #pattern == pin_moon_count" );
+
+    assert( 0 <= phase && phase < phase_count );
+
+    const int code = pattern[ phase ];
+
+    for ( int i = 0; i < pin_moon_count; ++i )
+    {
+        digitalWrite( pin_moon(i), bitRead( code, i ) );
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Date algorithm.
+// normalise() inspired on https://www.sourceware.org/git/gitweb.cgi?p=newlib-cygwin.git;a=blob_plain;f=newlib/libc/time/mktime.c
+
+#include <stdlib.h>     // for div_t; cstdlib not standard available on Arduino
+
+int is_leap_year( int const year )
+{
+    return ( 0 == year % 400 ) || (0 == year % 4 && 0 != year % 100 );
+}
+
+int days_in_month( int const year, int const month )
+{
+    enum { Jan, Feb, };
+
+    const int days[12] =
+    {
+        31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+    };
+
+    return days[ month ] + ( month == Feb && is_leap_year( year ) );
+}
+
+Date normalise( Date const date )
+{
+    Date d{ date.year, date.month - 1, date.day };
+
+    if ( d.month < 0 || d.month > 11 )
+    {
+        div_t res = div( d.month, 12 );
+
+        d.year += res.quot;
+
+        if ( ( d.month = res.rem ) < 0 )
+        {
+            d.month += 12;
+            --d.year;
+        }
+    }
+
+    if ( d.day <= 0 )
+    {
+        while ( d.day <= 0 )
+        {
+            if ( --d.month == -1 )
+            {
+                d.year--;
+                d.month = 11;
+            }
+            d.day += days_in_month( d.year, d.month );
+        }
+    }
+    else
+    {
+        while ( d.day > days_in_month( d.year, d.month ) )
+        {
+            d.day -= days_in_month( d.year, d.month );
+
+            if ( ++d.month == 12 )
+            {
+                d.year++;
+                d.month = 0;
+            }
+        }
+    }
+
+    return { d.year, d.month + 1, d.day };
+}
+
+Date make_date( Date const date, int const add_days )
+{
+    return normalise( { date.year, date.month, date.day + add_days } );
+}
+
+Date prev_date( Date date ) { return make_date( date, -1 ); }
+Date next_date( Date date ) { return make_date( date, +1 ); }
+
+// ----------------------------------------------------------------------------
+// Moon phase algorithm using integral numbers.
+// Simplified to work from 1900 to 2199 inclusive.
+// Based on Moon_phase(), http://www.voidware.com/moon_phase.htm
+
+enum MoonPhase
+{
+    New , WaxingCrescent, FirstQuarter, WaxingGibbous,
+    Full, WaningGibbous , ThirdQuarter, WaningCrescent,
+};
+
+int moon_phase( Date date )
+{
+    // Calculates the moon phase (0-7), accurate to 1 segment.
+    // 0 = > new moon.
+    // 4 => Full moon.
+
+    // Transfer months worth of days to day:
+
+    if ( date.month == 1 )
+    {
+        --date.day;
+    }
+    else if ( date.month == 2 )
+    {
+        date.day += 30;
+    }
+    else // m >= 3
+    {
+        date.day += 28 + ( date.month - 2 ) * 3059 / 100 + is_leap_year( date.year );
+    }
+
+    const int g = ( date.year - 1900 ) % 19 + 1;
+    const int e = ( 11 * g + 18 ) % 30;
+
+    const int adjust = ( (e == 25 && g > 11) || e == 24 );
+
+    return ( ( ( ( e + date.day + adjust ) * 6 + 11 ) % 177 ) / 22 ) % 8;
+}
+
+// ----------------------------------------------------------------------------
+// main program
+
+void sleep_ms( int ms )
+{
+    for ( int i = 0; i < ms; ++i )
+        delayMicroseconds( 1000 );
+}
+
+void setup()
+{
+    setup_date_display();
+    setup_moon_phase_display();
+}
+
+Date once( Date date, Pause pause_ms = display_blank_pause_ms )
+{
+    display_date( date, pause_ms );
+    display_moon_phase( moon_phase( date ) );
+
+    return next_date( date );
+}
+
+void loop()
+{
+    static Date date = START_DATE;
+
+    date = once( date );
+
+    sleep_ms( 1000 );
+}
+
+// end of file
---
+++ pictureEmbed23_old/pictureEmbed23_old.ino
@@ -0,0 +1,77 @@
+// ILI9341 example with embedded color bitmaps in sketch.
+// WILL NOT FIT ON ARDUINO UNO OR OTHER AVR BOARDS;
+// uses large bitmap image stored in array!
+
+// Options for converting images to the format used here include:
+//   http://www.rinkydinkelectronics.com/t_imageconverter565.php
+// or
+//  GIMP (https://www.gimp.org/) as follows:
+//    1. File -> Export As
+//    2. In Export Image dialog, use 'C source code (*.c)' as filetype.
+//    3. Press export to get the export options dialog.
+//    4. Type the desired variable name into the 'prefixed name' box.
+//    5. Uncheck 'GLIB types (guint8*)'
+//    6. Check 'Save as RGB565 (16-bit)'
+//    7. Press export to save your image.
+//  Assuming 'image_name' was typed in the 'prefixed name' box of step 4,
+//  you can have to include the c file, then using the image can be done with:
+//    tft.drawRGBBitmap(0, 0, image_name.pixel_data, image_name.width, image_name.height);
+//  See also https://forum.pjrc.com/threads/35575-Export-for-ILI9341_t3-with-GIMP
+
+#include "SPI.h"
+#include <Adafruit_ILI9341.h>
+//#include "dragon.h"
+//#include "dragon2.h"
+#include "ALL_Icons.h"
+
+// For the Adafruit shield, these are the default.
+//#define TFT_DC 9
+//#define TFT_CS 10
+
+// Feather 32u4 or M0 with TFT FeatherWing:
+#define TFT_DC 10
+#define TFT_CS  9
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// If using the breakout, change pins as desired
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+void setup() {
+  tft.begin();
+}
+
+void loop(void) {
+
+    tft.setRotation(1);
+    tft.fillScreen(ILI9341_BLACK);
+    //var.drawBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h, uint16_t color);
+//    tft.drawRGBBitmap(random(-DRAGON_WIDTH , tft.width()),random(-DRAGON_HEIGHT, tft.height()),(uint16_t *)dragonBitmap2,DRAGON_WIDTH, DRAGON_HEIGHT);
+    delay(3000);
+//  tft.drawRGBBitmap(random(-L0_WIDTH , tft.width()),random(-L0_HEIGHT, tft.height()),(uint16_t *)L0,L0_WIDTH, L0_HEIGHT);
+//  tft.drawRGBBitmap(random(-L1_WIDTH , tft.width()),random(-L1_HEIGHT, tft.height()),(uint16_t *)L1,L1_WIDTH, L1_HEIGHT);
+//  tft.drawRGBBitmap(random(-L2_WIDTH , tft.width()),random(-L2_HEIGHT, tft.height()),(uint16_t *)L2,L2_WIDTH, L2_HEIGHT);
+ // tft.drawRGBBitmap(random(-L3_WIDTH , tft.width()),random(-L3_HEIGHT, tft.height()),(uint16_t *)L3,L3_WIDTH, L3_HEIGHT);
+//  tft.drawRGBBitmap(random(-L4_WIDTH , tft.width()),random(-L4_HEIGHT, tft.height()),(uint16_t *)L4,L4_WIDTH, L4_HEIGHT);
+//  tft.drawRGBBitmap(random(-L5_WIDTH , tft.width()),random(-L5_HEIGHT, tft.height()),(uint16_t *)L5,L5_WIDTH, L5_HEIGHT);
+//  tft.drawRGBBitmap(random(-L6_WIDTH , tft.width()),random(-L6_HEIGHT, tft.height()),(uint16_t *)L6,L6_WIDTH, L6_HEIGHT);
+//  tft.drawRGBBitmap(random(-L7_WIDTH , tft.width()),random(-L7_HEIGHT, tft.height()),(uint16_t *)L7,L7_WIDTH, L7_HEIGHT);
+//  tft.drawRGBBitmap(random(-L8_WIDTH , tft.width()),random(-L8_HEIGHT, tft.height()),(uint16_t *)L8,L8_WIDTH, L8_HEIGHT);
+//  tft.drawRGBBitmap(random(-L9_WIDTH , tft.width()),random(-L9_HEIGHT, tft.height()),(uint16_t *)L9,L9_WIDTH, L9_HEIGHT);
+//  tft.drawRGBBitmap(random(-L10_WIDTH , tft.width()),random(-L10_HEIGHT, tft.height()),(uint16_t *)L10,L10_WIDTH, L10_HEIGHT);
+//  tft.drawRGBBitmap(random(-L11_WIDTH , tft.width()),random(-L11_HEIGHT, tft.height()),(uint16_t *)L11,L11_WIDTH, L11_HEIGHT);
+//  tft.drawRGBBitmap(random(-L12_WIDTH , tft.width()),random(-L12_HEIGHT, tft.height()),(uint16_t *)L12,L12_WIDTH, L12_HEIGHT);
+//  tft.drawRGBBitmap(random(-L13_WIDTH , tft.width()),random(-L13_HEIGHT, tft.height()),(uint16_t *)L13,L13_WIDTH, L13_HEIGHT);
+//  tft.drawRGBBitmap(random(-L14_WIDTH , tft.width()),random(-L14_HEIGHT, tft.height()),(uint16_t *)L14,L14_WIDTH, L14_HEIGHT);
+//  tft.drawRGBBitmap(random(-L15_WIDTH , tft.width()),random(-L15_HEIGHT, tft.height()),(uint16_t *)L15,L15_WIDTH, L15_HEIGHT);
+//  tft.drawRGBBitmap(random(-L16_WIDTH , tft.width()),random(-L16_HEIGHT, tft.height()),(uint16_t *)L16,L16_WIDTH, L16_HEIGHT);
+//  tft.drawRGBBitmap(random(-L17_WIDTH , tft.width()),random(-L17_HEIGHT, tft.height()),(uint16_t *)L17,L17_WIDTH, L17_HEIGHT);
+//  tft.drawRGBBitmap(random(-L18_WIDTH , tft.width()),random(-L18_HEIGHT, tft.height()),(uint16_t *)L18,L18_WIDTH, L18_HEIGHT);
+//  tft.drawRGBBitmap(random(-L19_WIDTH , tft.width()),random(-L19_HEIGHT, tft.height()),(uint16_t *)L19,L19_WIDTH, L19_HEIGHT);
+//  tft.drawRGBBitmap(random(-L20_WIDTH , tft.width()),random(-L20_HEIGHT, tft.height()),(uint16_t *)L20,L20_WIDTH, L20_HEIGHT);
+//  tft.drawRGBBitmap(random(-L21_WIDTH , tft.width()),random(-L21_HEIGHT, tft.height()),(uint16_t *)L21,L21_WIDTH, L21_HEIGHT);
+//  tft.drawRGBBitmap(random(-L22_WIDTH , tft.width()),random(-L22_HEIGHT, tft.height()),(uint16_t *)L22,L22_WIDTH, L22_HEIGHT);
+ // tft.drawRGBBitmap(random(-L23_WIDTH , tft.width()),random(-L23_HEIGHT, tft.height()),(uint16_t *)L23,L23_WIDTH, L23_HEIGHT);
+  tft.drawRGBBitmap(random(-LE_WIDTH , tft.width()),random(-LE_HEIGHT, tft.height()),(uint16_t *)LE,LE_WIDTH, LE_HEIGHT);
+  delay(3000);
+  
+}
